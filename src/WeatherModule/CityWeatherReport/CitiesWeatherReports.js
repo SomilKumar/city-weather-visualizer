a0a1d3597d43d2619a7367c95a7bb5d9e41fd43d
Somil Kumar <somil.kumar91@gmail.com>
2019-02-25 11:56:48 +0100

    Adding city weather report component

---
+++ src/WeatherModule/CityWeatherReport/CitiesWeatherReports.js
@@ -0,0 +1,65 @@
+import React, {
+    useState, useEffect
+} from 'react';
+
+
+
+import CityWeather from './CityWeather';
+import './CitiesWeartherReport.css';
+
+function CitiesWeatherReport({allCities}) {
+    
+    const [cities, setCities] = useState([]);
+    const [activeTab, setActiveTab] = useState(0);
+    
+    useEffect(() => {        
+        if(allCities.length) {
+            console.log("Received Cities", allCities);
+            setCities(allCities);
+            setActiveTab(allCities.length -1);
+        }
+      }, 
+      [allCities]
+    );
+
+    return ( 
+        <React.Fragment>
+        <div className="columns is-centered">  
+            <div className="column is-half">
+
+            {
+               cities.length === 0 ? <p>No City Selected</p> :
+                <React.Fragment>
+                <div className="tabs">
+                    <ul>
+                   {cities.map((e, index) => (
+                        <li className={`${index === activeTab ? 'is-active': ''}`} 
+                            key={e.id}>
+                            <a onClick={() => setActiveTab(index)}>
+                                {e.name}
+                            </a>
+                        </li>
+                    ))
+                   }
+                    </ul>
+                </div>
+                
+                <div style={{display: "flex", flexDirection: "row"}}>
+                { cities.map((e, index) => (
+                    <div className={`${index === activeTab ? 'visible': 'hidden'}`} 
+                        key={e.id}>
+                        <CityWeather cityData={e}/>
+                    </div>
+                ))
+                }
+                </div>
+                </React.Fragment>
+            
+            } 
+            </div>
+        </div>
+        </React.Fragment>
+    )
+}
+
+export default CitiesWeatherReport;
---
+++ src/WeatherModule/CityWeatherReport/CityWeather.js
@@ -0,0 +1,126 @@
+import React, {useState, useEffect} from 'react';
+
+
+
+
+function CityWeather({cityData}) {
+
+    const [tempUnit, setTempUnit] = useState('metric');
+    const [isLoading, setIsLoading] = useState(false);
+    const [cityForecast, setCityForecast] = useState({});
+    const [weekMood, setWeekMood] = useState('');
+    const [fetchError, setFetchError] = useState(false)
+
+    useEffect( () => {
+        setIsLoading(true);
+        fetchCityForecast();
+    },
+        [cityData,tempUnit]
+    );
+
+    function calculateWeekMood(allDays) {
+        const allCodes= {
+            great: [800],
+            normal:[300,500,501,701,711,721,801,802,803],
+            bad:[200,201,210,211,301,502,503,504,511,600,601,731,761,804]
+        };
+        let avg ={
+            great:{label: "Great", count:0},
+            normal:{label: "Normal", count:0},
+            bad:{label: "Bad", count:0},
+            unacceptable:{label: "Unacceptable", count:0}
+        }
+        let mood = {label: "Great"};
+        let max = 0;
+
+        allDays.forEach(el => {
+            if(allCodes.great.indexOf(el.weather[0].id))
+                avg.great.count+= 1;
+            else if(allCodes.normal.indexOf(el.weather[0].id))
+                avg.normal.count+=1;
+            else if(allCodes.bad.indexOf(el.weather[0].id))
+                avg.bad.count+=1;
+            else 
+                avg.unacceptable.count+=1;
+        });
+   
+        for(const key in avg) {
+            if(avg[key].count > max ) {
+                mood = avg[key];
+            }
+        }
+
+        console.log(avg, mood);
+
+        setWeekMood(mood.label);
+
+    }
+
+    async function fetchCityForecast() {
+        let data = [];
+        const forecastQuery = `http://api.openweathermap.org/data/2.5/forecast?id=${cityData.id}&units=${tempUnit}&APPID=a6c98ebcf46fd073bf700219a88bd003`;
+        try {
+            const response = await fetch(forecastQuery);
+            data = await response.json();
+            console.log("City Data for",data);
+            setCityForecast(data);
+            calculateWeekMood(data.list);
+            setIsLoading(false);
+            setFetchError(false);
+        } catch(error) {
+            console.log("Error Handled",error);
+            setIsLoading(false);
+            setFetchError(true);
+        }
+    }
+    
+    return (
+        <div className="columns">  
+            <div className="column is-half">
+            {
+                isLoading && 
+                <div>
+                    <span>Loading Weather Report.....</span> <br/> <br />
+                    <button className="button is-fullwidth is-link is-loading is-rounded"></button>
+                </div>
+            }
+            {
+                fetchError && 
+                <div>
+                    <span> &emsp; Error while fetching weather data.... Please try again.</span>
+                </div>
+            }
+            {
+                Object.keys(cityForecast).length >0 && 
+                <React.Fragment>
+                <div className="columns">
+                    <div className="column is-6">
+                        <span className="tag is-large"> Mood of week: {weekMood}</span> 
+                    </div>
+                    
+                    <div className="column is-6">
+                        <div className="control">
+                            <div className="select">
+                                <select onChange={(e) => setTempUnit(e.target.value)}>
+                                    <option value="metric">Temp in Celsius</option>
+                                    <option value="imperial">Temp in Fahrenheit</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            
+                <div className="columns">
+                    <div className="column is-12">
+                        
+                    </div>
+                </div>
+                </React.Fragment>
+            }
+            </div>
+        </div>
+    )
+}
+
+export default CityWeather;
